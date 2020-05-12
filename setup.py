5a3e9ea839c14f54dd53f304f3c43406bd67ee5b
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-05-13 00:08:44 +0900

    Update: setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='discordbotjp',
-    version='2020.3.21',
+    version='2020.5.13',
     py_modules=find_packages(),
     install_requires=[
-        'discord.py >= 1.2.5',
-        'dispander >= 0.3.0',
+        'discord.py >= 1.3.3',
+        'dispander >= 0.4.0',
         'echidna >= 0.2.1',
     ],
 )
