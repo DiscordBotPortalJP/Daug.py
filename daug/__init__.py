67f4d0050587ce9673c6515adfeb0f90f4cdbc24
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-05-13 11:15:06 +0900

    Bugfix

---
+++ daug/__init__.py
@@ -0,0 +1,3 @@
+from daug.extension import setup
+
+__all__ = ['setup']
--- daug/extension.py
+++ daug/extension.py
@@ -1,7 +1,8 @@
-from daug.cogs.thread import Thread
+from daug.cogs.favorite import Favorite
 from daug.cogs.join import Join
 from daug.cogs.leave import Leave
-from daug.cogs.favorite import Favorite
+from daug.cogs.thread import Thread
+from daug.cogs.utils import Utils
 
 
 def setup(bot):
@@ -9,3 +10,4 @@ def setup(bot):
     bot.add_cog(Join(bot))
     bot.add_cog(Leave(bot))
     bot.add_cog(Favorite(bot))
+    bot.add_cog(Utils(bot))
