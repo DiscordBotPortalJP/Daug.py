29a9a91bc13a240e4f5a931a9b178f76a676930a
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-08-05 22:08:01 +0900

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='daug',
-    version='2020.5.13',
+    version='2020.8.5',
     py_modules=find_packages(),
     install_requires=[
         'discord.py >= 1.3.3',
