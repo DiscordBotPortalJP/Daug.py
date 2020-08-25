28f0242b40409eed41cdf20a81ac2105a2961bd6
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-08-26 00:30:36 +0900

    Update: setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='Daug',
-    version='2020.8.25',
-    py_modules=find_packages(),
+    version='2020.8.26',
+    author='1ntegrale9',
+    author_email='1ntegrale9uation@gmail.com',
+    description='discord.py を利用した Discord Bot 向け機能拡張ライブラリ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DiscordBotPortalJP/Daug.py',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
     install_requires=[
         'discord.py >= 1.4.1',
         'dispander >= 0.4.0',
