caaff4e90db860a90bfb778308d06d256dd4fec3
1ntegrale9 <1ntegrale9uation@gmail.com>
2021-08-22 17:07:15 +0900

    [Refactor] ローカル検証用のスクリプトをリファクタリング

--- testbot.py
+++ testbot.py
@@ -1,36 +1,37 @@
-import discord
-from discord.ext import commands
+from discord import Intents
+from discord.ext.commands import Bot
+from discord.ext.commands import when_mentioned_or
+from os import getenv
 import logging
-import os
 
-config = {
-    'Daug': {
-        'guild_id': 709729198075412601,
-        'guild_logs_id': 674500858054180874,
-        'role_bot_limited_id': 710758653321281597,
-        'role_member_id': 709729198075412607,
-        'role_contributor_id': 709729198075412609,
-        'channel_tips_id': 709729198809415716,
-        'category_issues_id': 709729199228846140,
-        'category_open_id': 709729199509733383,
-        'category_closed_id': 709729199954329679,
-        'category_archive_id': 709729216245006457,
-    },
-}
+
+def run(config, extensions):
+    logging.basicConfig(level=logging.WARNING)
+    bot = Bot(
+        command_prefix=when_mentioned_or('$'),
+        help_command=None,
+        intents=Intents.all(),
+    )
+    bot.config = config
+    for extension in extensions:
+        bot.load_extension(f'Daug.extensions.{extension}')
+    bot.run(getenv('DISCORD_BOT_TOKEN'))
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARNING)
-    bot = commands.Bot(
-        command_prefix='/',
-        help_command=None,
-        intents=discord.Intents.all(),
-    )
-    token = os.environ['DISCORD_BOT_TOKEN']
-    bot.config = config
-    bot.load_extension('Daug.extensions.channels')
-    bot.load_extension('Daug.extensions.favorite')
-    bot.load_extension('Daug.extensions.join')
-    bot.load_extension('Daug.extensions.leave')
-    bot.load_extension('Daug.extensions.thread')
-    bot.load_extension('Daug.extensions.utils')
-    bot.run(token)
+    config = {
+        'Daug': {
+            'guild_id': 709729198075412601,
+            'guild_logs_id': 674500858054180874,
+            'role_bot_limited_id': 710758653321281597,
+            'role_member_id': 709729198075412607,
+            'role_contributor_id': 709729198075412609,
+            'channel_tips_id': 709729198809415716,
+            'category_issues_id': 709729199228846140,
+            'category_open_id': 709729199509733383,
+            'category_closed_id': 709729199954329679,
+            'category_archive_id': 709729216245006457,
+        },
+    }
+    extensions = ('channels', 'favorite', 'join', 'leave', 'thread', 'utils')
+    run(config, extensions)
