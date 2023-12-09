288c5ff76c4b19838f78deb02b8276da33d35eaf
shun <1ntegrale9uation@gmail.com>
2023-12-09 18:17:54 +0900

    bugfix (function extract_channel_mentions)

--- daug/utils/__init__.py
+++ daug/utils/__init__.py
@@ -26,7 +26,7 @@ def extract_role_mentions(guild: discord.Guild, text: str) -> list[discord.Role]
 
 
 def extract_channel_mentions(guild: discord.Guild, text: str) -> list[discord.abc.GuildChannel]:
-    return [guild.get_channel(int(x)) for x in re.findall(r'<@&([0-9]{15,20})>', text) if guild.get_channel(int(x))]
+    return [guild.get_channel(int(x)) for x in re.findall(r'<@#([0-9]{15,20})>', text) if guild.get_channel(int(x))]
 
 
 def get_related_tc(voice_channel) -> discord.TextChannel:
